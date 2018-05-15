f357d2cde5c02759518ca0c815bc0aa28eaccf1a
Matthew Wareing <matthewwareing@Matthews-MacBook-Pro.local>
2018-05-15 17:59:09 +0100

    Challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,11 @@
 class DockingStation
-  def initialize
-  end
+  attr_reader :bike
 
   def release_bike
+    Bike.new
+  end
+
+  def dock bike
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,13 @@ describe DockingStation do
   it 'exists' do
     expect(DockingStation.new).to be_an_instance_of DockingStation
   end
-
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
+  
+  it 'returns the docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
